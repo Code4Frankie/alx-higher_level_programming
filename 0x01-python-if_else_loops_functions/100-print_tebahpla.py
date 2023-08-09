cdd91eba4ccd0b6284979a39d0451d076cbe9f7d
Code4Frankie <frankiecodesolutions@gmail.com>
2023-08-08 17:07:23 -0700

    up to date

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for x in range(26):
-    if x % 2 == 0:
-        print('{:c}'.format(122 - x), end='')
+for i in range(26):
+    if i % 2 == 0:
+        print('{:c}'.format(122 - i), end='')
     else:
-        print('{:c}'.format(90 - x), end='')
-
+        print('{:c}'.format(90 - i), end='')
